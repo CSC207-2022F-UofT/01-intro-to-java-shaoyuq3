e4b6acc9af0c69502dd87f5dbf52d5fdaeb5c92e
shaoyuq3 <yuqing.shao@mail.utoronto.ca>
2022-10-08 18:04:04 -0400

    Follow TODO, done the task.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -63,7 +63,7 @@ public class Basics {
          *                value 100.
          */
 
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -99,7 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int curr = 10; curr >= 0; curr--){
+            System.out.println("Current count: " + curr);
+        }
 
     }
 
@@ -133,6 +135,7 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
+
         /* TODO (Task 4): Complete this method body.
          *                The String methods .split and .charAt may be helpful,
          *                along with the StringBuilder.append
@@ -141,6 +144,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] temp = to_split.split(" ");
+        for (String s : temp) {
+            ret.append(s.charAt(0));
+        }
+
 
         // Fill in the rest of the body here
 
@@ -170,7 +178,11 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        if (arr.length > 1) {
+            for (int i = 1; i < arr.length; i += 2) {
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
